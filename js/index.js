4f2f1e1e6d2ff55c78acff69d8686e3206c8ac26
Jesus C Guerrero <jesusclementguerrero@gmail.com>
2020-03-10 18:03:16 -0400

    Finished assignment

--- js/index.js
+++ js/index.js
@@ -1,1 +1,46 @@
 // Your code goes here
+let variable1 = document.querySelector('body')
+let variable2 = document.querySelector('header')
+let variable3 = document.querySelector('.text-content')
+let variable4 = document.querySelector('img')
+let sound = document.querySelector('audio')
+
+variable1.addEventListener('mouseover', (event) => {
+    variable1.style.backgroundColor = 'dodgerblue';
+}) 
+
+variable1.addEventListener('mouseout', (event) => {
+    variable1.style.backgroundColor = 'white';
+}) 
+
+variable2.addEventListener('drag', (event) => {
+    variable2.style.backgroundColor = 'red';
+})
+
+variable2.addEventListener('dragend', (event) => {
+    variable2.style.backgroundColor = 'white';
+})
+
+variable1.addEventListener('dblclick', (event) => {
+    alert('Attention. This is an important message from the FBI. Sensitive content has been found on the device of user Dustin Graham. An team is on route to your home in Albuquerque, New Mexico. Please remain where you are.');
+})
+
+variable1.addEventListener('wheel', (event) => {
+    variable1.style.backgroundColor = 'pink';
+})
+
+variable1.addEventListener('keydown', (event) => {
+    sound.play();
+})
+
+variable1.addEventListener('copy', (event) => {
+    variable1.style.color = 'purple';
+})
+
+variable4.addEventListener('click', (event) => {
+    variable1.style.backgroundColor = 'black';
+})
+
+variable2.addEventListener('mousedown', (event) => {
+    variable1.style.backgroundColor = 'orange';
+})
